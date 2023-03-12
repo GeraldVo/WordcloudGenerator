6815074fe37be7d6661e3b1d11d19b9738b0b0e9
GeraldVo <gerald.votzi@gmail.com>
2023-03-12 19:51:55 +0100

    add authorization and backend and frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes/routes');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+app.use(cors());
+const session = require('express-session');
+const http = require('http').Server(app);
+const io = require('./socket');
+const port = process.env.PORT || 3001;
 
 
+
+app.use(session({
+  secret: 'mysecret', // change this to a unique secret key
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+/*
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+*/
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
+//#region 
 // Define Swagger options
 const swaggerOptions = {
   swaggerDefinition: {
@@ -24,14 +64,49 @@ const swaggerOptions = {
   apis: ['./Backend/routes/*.js'] // Path to the API routes
 
 };
-
 // Initialize Swagger-jsdoc
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-
 // Serve the Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+//#endregion
 app.use('/', routes);
+
+
+/*
+
+io.use((socket, next) => {
+  const joinCode = socket.handshake.query.joinCode;
+  // Check the validity of the join code here
+  if (joinCode === 'valid_join_code') {
+    return next();
+  }
+  // If the join code is not valid, reject the connection
+  return next(new Error('Invalid join code'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    console.log("CHat received");
+    io.emit('chat message', msg);
+  });
+});
+
+
+*/
+
+io.attach(http);
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
